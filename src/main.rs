9011b9e882978264369b22a50368ea4ba3968c8a
Avenegy <avenegydmytro@gmail.com>
2026-05-17 16:49:05 +0300

    feat: add task loop and command parsing
    - add command list and exit - add HashMap storage for active tasks - add 'add' command features - add 'list' command features

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
 use std::io;
+use std::collections::HashMap;
 
 fn main() {
     println!("TODO LIST");
@@ -11,27 +12,38 @@ fn main() {
     //     "edit",
     // ];
 
-    struct Tasks {
-        name: String,
-        done: bool,
-        id: i32,
-    }
+    let mut active_task: HashMap<i32, Task>  = HashMap::new();
 
-    let mut promt: String = String::from("");
-    let _ = io::stdin().read_line(&mut promt);
-    let trimmed = promt.trim();
-    
-    match trimmed {
-        "add" => add_task(),
-        "done" => done_task(),
-        "remove" => remove_task(),
-        "edit" => edit_task(),
-        _ => println!("Idk this command bro")
+    loop {
+        let mut promt: String = String::from("");
+        let _ = io::stdin().read_line(&mut promt);
+        
+
+        let parts: Vec<&str> = promt.split_whitespace().collect();
+        let rest = &parts[1..];
+        let name = rest.join(" ");
+
+        match parts[0] {
+            "add" => add_task(&mut active_task, name),
+            "done" => done_task(),
+            "remove" => remove_task(),
+            "edit" => edit_task(),
+            "list" => list_task(&mut active_task),
+            "exit" => break,
+            _ => println!("Idk this command bro"),
+        }
     }
 }
 
-fn add_task(){
-    
+struct Task {
+       name: String,
+       done: bool,
+    }
+
+fn add_task(tasks: &mut HashMap<i32, Task>, name: String) {
+    let id_unsize = &tasks.len() + 1;
+    let id = id_unsize as i32;
+    tasks.insert(id, Task{name: name, done: false});
 }
 
 fn done_task(){
@@ -43,5 +55,18 @@ fn remove_task(){
 }
 
 fn edit_task(){
+
+}
+
+
+fn list_task(tasks: &HashMap<i32, Task>){
+    for (id, val) in tasks {
+        let mut mark_done = String::from("[ ]");
+        let name = &val.name;
+        if val.done{
+           mark_done = String::from("[x]");
+        }
+        println!("{id}. {name} {mark_done}");
+    }
 
 }
